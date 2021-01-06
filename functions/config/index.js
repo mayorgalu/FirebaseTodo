ceb16d263363c1df214067da48d9f5d92b404ea4
mayorgalu <mayorgalu@estrategy.tech>
2021-01-06 13:48:46 -0400

    Initialize project

---
+++ functions/config/index.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBJgg9n1A9t0qCJ4ajVXC9GYE825kkM7B8",
+  authDomain: "todoapi-bc36c.firebaseapp.com",
+  projectId: "todoapi-bc36c",
+  storageBucket: "todoapi-bc36c.appspot.com",
+  messagingSenderId: "988194779642",
+  appId: "1:988194779642:web:ead4324e78f64a460d3443",
+  measurementId: "G-Q940Q9ZW10",
+};
+module.exports = firebaseConfig;
